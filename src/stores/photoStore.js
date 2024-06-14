247c72a39ba7a67f46583d1896a4cd0f09f0edd7
RiskixBuulolo <riski01.pku@gmail.com>
2024-06-14 22:49:48 +0700

    UAS PROJECT

--- src/stores/photoStore.js
+++ src/stores/photoStore.js
@@ -7,7 +7,7 @@ export const usePhotoStore = defineStore('photo', {
   actions: {
     async fetchPhotos() {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/photos')
+        const response = await fetch('https://jsonplaceholder.typicode.com/albums')
         this.photos = await response.json()
       } catch (error) {
         console.error('Error fetching photo data:', error)
